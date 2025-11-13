4a6a26ea5b7f1c20ebe52f981c9e100d7bdabdb5
Reander Agulto <reanderagulto29@gmail.com>
2025-11-13 13:34:50 +0800

    Product Slider

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+(function (w, d, h, b){
+    
+    const productSlider = () => {
+        d.querySelectorAll('#product-slider', () => {
+            
+        });
+    }
+
+    d.addEventListener('DOMContentLoaded', () => {
+        productSlider();
+    });
+    
+})(window, document, document.documentElement, document.body);
