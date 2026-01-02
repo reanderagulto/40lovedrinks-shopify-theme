4dd73b689a0d8cadf7b86e2809937b11402c6ae7
Reander Agulto <reanderagulto29@gmail.com>
2026-01-02 19:13:43 +0800

    Updates on Product Details Page

--- assets/product.js
+++ assets/product.js
@@ -12,7 +12,17 @@
       });
     }
   };
+  productOptions = () => {
+    const optionButtons = d.querySelectorAll(".product__purchase-options--buttons .btn");
+    optionButtons.forEach((button) => {
+      button.addEventListener("click", function() {
+        optionButtons.forEach((btn) => btn.classList.remove("active"));
+        this.classList.add("active");
+      });
+    });
+  };
   d.addEventListener("DOMContentLoaded", function() {
     productDetails();
+    productOptions();
   });
 })(window, document);
